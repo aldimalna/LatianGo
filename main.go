858f545b30a2a045a425327f02a239b8f9a9c378
aldimalna <aldim222@gmail.com>
2023-02-26 17:44:16 +0700

    add go.sum , modified go.mod, add more line

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title   string `json:"title"`
+	Descr   string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "welcome to the Homepage!")
@@ -13,11 +27,16 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
-	fmt.Println("The server is On")
+	fmt.Println("Server is starting.......")
+	Articles = []Article{
+		{Title: "hello", Descr: "Article description", Content: "Article Content"},
+		{Title: "hello2", Descr: "Article description", Content: "Article Content"},
+	}
 	handleRequests()
 
 }
