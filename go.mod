8df12213e3c45620a56eb92e9f8b1301eff4c97f
aldimalna <aldim222@gmail.com>
2023-02-26 17:07:33 +0700

    Add more line

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goApi
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "welcome to the Homepage!")
+	fmt.Println("endpoint hit: homepage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	fmt.Println("The server is On")
+	handleRequests()
+
+}
