f4fac6e53e982c8fd3166a71aff61d6ccdb241eb
aldimalna <aldim222@gmail.com>
2023-02-26 18:12:39 +0700

    Change for chi

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "welcome to the Homepage!")
-	fmt.Println("endpoint hit: homepage")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":10000", nil))
-}
-
 func main() {
-	fmt.Println("The server is On")
-	handleRequests()
+	fmt.Println("Server is starting.......")
 
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/hello-world", getHandler)
+	r.Get("/api/auth", userAuth)
+	fmt.Println("Server is listening on port 10000...")
+	log.Fatal(http.ListenAndServe(":10000", r))
+
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("hello world")
+}
+
+func userAuth(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("this is user authentication apge")
 }
